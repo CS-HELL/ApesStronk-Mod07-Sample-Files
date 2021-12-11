ce357b272d6feb827bf7504ccd414a0dfe3bb663
JEREMY MADRIAGA <2200093@slu.edu.ph>
2021-12-11 19:55:46 +0800

    Updated main.cpp - Solve Digits Sum Method - User Choices

---
+++ cmake-build-debug/CMakeFiles/ApesStronk_Mod07_Sample_Files.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ApesStronk_Mod07_Sample_Files.exe"
+  "ApesStronk_Mod07_Sample_Files.exe.manifest"
+  "ApesStronk_Mod07_Sample_Files.lib"
+  "ApesStronk_Mod07_Sample_Files.pdb"
+  "CMakeFiles/ApesStronk_Mod07_Sample_Files.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ApesStronk_Mod07_Sample_Files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,76 @@
+//
+// Created by Jeremy on 12/11/2021.
+//
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+void solveDigitsSum(){
+    cout << "\nDigits Sum Problem" << endl;
+    int t = 0; // Will be the total input accepted by the loop.
+    int input = 0; // Will be the input that will be stored in an array.
+
+    cin >> t;
+    int* inputs = new int[t]; // Array of inputs.
+
+    for(int i = 0; i < t; i++){
+        cin >> input;
+        inputs[i] = input;
+    }
+
+    for(int i = 0; i < t; i++){
+        int k = inputs[i];
+        int j = (k+1)/10;
+        std::cout << j << " ";
+    }
+    std::cout << endl;
+}
+
+void solveDominoPiling(){
+    cout << "\nDomino Piling" << endl;
+    int M, N;
+    cin >> M >> N;
+    cout << M * N / 2 << endl;
+}
+
+
+
+void displayUserChoices() {
+    cout << "\n[CodeForces Problem Set by ApesStronk]\n"
+            "1. Digits Sum\n"
+            "2. Domino Piling\n"
+            "3. \n"
+            "0. Exit Program.\n"
+            "Your Choice:";
+}
+
+void initiateUserChoice(int userChoice){
+    switch(userChoice){
+        case 1: solveDigitsSum();
+            break;
+        case 2: solveDominoPiling();
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 0: cout << "Goodbye.";
+            exit(3);
+        default:
+            cout << "Invalid Input.";
+    }
+}
+
+int main() {
+    int userChoice;
+
+    for (;;) {
+        displayUserChoices();
+        cin >> userChoice;
+
+        initiateUserChoice(userChoice);
+    }
+}
